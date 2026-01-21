bf9e590947624b1901685dbef83b250b9918b366
medhruba <dhrubarajchaudhary498@gmail.com>
2026-01-21 15:14:26 +0545

    Data Transformation

--- src/logger.py
+++ src/logger.py
@@ -15,5 +15,5 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-# if __name__ == "__main__":
-#     logging.info('Logging has Started')
+if __name__ == "__main__":
+    logging.info('Logging has Started')
