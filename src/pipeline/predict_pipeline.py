058f675ac7be015a850180a0ffded6515fd2319c
medhruba <dhrubarajchaudhary498@gmail.com>
2026-01-23 15:51:35 +0545

    Predicting the result and Using Docker

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -39,7 +39,7 @@ class CustomData:
         self.race_ethnicity = race_ethnicity
         self.parental_level_of_education = parental_level_of_education
         self.lunch = lunch
-        self.test_preparation_score = test_preparation_course
+        self.test_preparation_course = test_preparation_course
         self.reading_score = reading_score
         self.writing_score = writing_score
 
@@ -50,7 +50,7 @@ class CustomData:
                 "race_ethnicity": [self.race_ethnicity],
                 "parental_level_of_education": [self.parental_level_of_education],
                 "lunch": [self.lunch],
-                "test_preparation_course": [self.test_preparation_score],
+                "test_preparation_course": [self.test_preparation_course],
                 "reading_score": [self.reading_score],
                 "writing_score": [self.writing_score]
             }
